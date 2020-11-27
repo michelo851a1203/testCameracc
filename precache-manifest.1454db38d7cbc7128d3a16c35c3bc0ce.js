1fb6dc676f848c8bed07e48b2a953178a7713392
MichaelHo <michelo851a1203@gmail.com>
2020-11-27 17:36:19 +0800

    deploy

---
+++ precache-manifest.1454db38d7cbc7128d3a16c35c3bc0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbd5c4b0515f6bd32f8743d1233f6b0",
+    "url": "/testCameracc/index.html"
+  },
+  {
+    "revision": "7ecb464616be9897df40",
+    "url": "/testCameracc/js/about.b5b343e4.js"
+  },
+  {
+    "revision": "9fb4ae621d5e457e05c2",
+    "url": "/testCameracc/js/app.5c2ce869.js"
+  },
+  {
+    "revision": "26afb52f0ef58b113e26",
+    "url": "/testCameracc/js/chunk-vendors.8741a838.js"
+  },
+  {
+    "revision": "9d300553d41a22cc680c4ec9a06d4f19",
+    "url": "/testCameracc/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testCameracc/robots.txt"
+  }
+]);
